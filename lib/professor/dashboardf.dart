e5752e231e877d25ed249162b0772803a8d1190a
Asfan45 <103164775+Asfan45@users.noreply.github.com>
2022-04-08 09:51:59 +0530

    Add files via upload

---
+++ lib/professor/dashboardf.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:demo2/professor/loginf.dart';
+
+class dashboardf extends StatefulWidget {
+  const dashboardf({Key? key}) : super(key: key);
+
+  @override
+  _dashboardfState createState() => _dashboardfState();
+}
+
+class _dashboardfState extends State<dashboardf> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
+
